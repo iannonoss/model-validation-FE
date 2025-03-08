4d2c505545077e01f02ddf0b2f4fd8412e671648
iannonss <iannonemattia7@gmai.com>
2025-03-08 18:40:37 +0100

    wip: first commit new dashboard to insert csv and manage data to send to backend

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,78 @@
+import {Component} from '@angular/core';
+import {NgForOf, NgIf} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  standalone: true,
+  imports: [
+    NgIf,
+    FormsModule,
+    NgForOf
+  ],
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  fileName: string | null = null;
+  fileSize: string | null = null;
+  columns: string[] = [];
+  independentVars: string[] = [];
+  dependentVar: string | null = null;
+  selectedModel: string | null = null;
+
+  models = ['Random Forest', 'SVM', 'XGBoost', 'Neural Network'];
+
+  public onFileSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      this.fileName = file.name;
+      this.fileSize = this.formatFileSize(file.size);
+      this.extractColumns(file);
+    }
+  }
+
+  public onDragOver(event: DragEvent) {
+    event.preventDefault();
+  }
+
+  public onDrop(event: DragEvent) {
+    event.preventDefault();
+    const file = event.dataTransfer?.files[0];
+    if (file) {
+      this.fileName = file.name;
+      this.fileSize = this.formatFileSize(file.size);
+      this.extractColumns(file);
+    }
+  }
+
+  public extractColumns(file: File) {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const text = (e.target?.result as string) || '';
+      const lines = text.split('\n');
+      this.columns = lines[0].split(',').map(col => col.trim());
+    };
+    reader.readAsText(file);
+  }
+
+  public toggleIndependentVar(column: string, event: any) {
+    if (event.target.checked) {
+      this.independentVars.push(column);
+    } else {
+      this.independentVars = this.independentVars.filter(varName => varName !== column);
+    }
+  }
+
+  private formatFileSize(size: number): string {
+    if (size < 1024) {
+      return size + ' B'; // Byte
+    } else if (size < 1024 * 1024) {
+      return (size / 1024).toFixed(2) + ' KB'; // Kilobyte
+    } else if (size < 1024 * 1024 * 1024) {
+      return (size / (1024 * 1024)).toFixed(2) + ' MB'; // Megabyte
+    } else {
+      return (size / (1024 * 1024 * 1024)).toFixed(2) + ' GB'; // Gigabyte
+    }
+  }
+}
