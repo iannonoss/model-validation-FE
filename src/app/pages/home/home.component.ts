03c1b6560cafbb80f8955cf10d1f15f61dc890bf
iannonoss <iannonemattia7@gmail.com>
2025-03-13 17:56:12 +0100

    wip: (preprocessing operation) now front end project contain the information to send to backend.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,11 @@
-import {Component} from '@angular/core';
-import {NgForOf, NgIf} from '@angular/common';
+import {Component, inject} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {SharedService} from '../../services/shared.service';
 import {catchError, Subscription, tap, throwError} from 'rxjs';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +13,9 @@ import {catchError, Subscription, tap, throwError} from 'rxjs';
   standalone: true,
   imports: [
     FormsModule,
+    MatFormFieldModule,
+    MatSidenavModule,
+    MatButtonModule,
   ],
   styleUrls: ['./home.component.css']
 })
@@ -22,9 +28,25 @@ export class HomeComponent {
   public selectedModel: string | null = null;
   public models = ['Random Forest', 'SVM', 'XGBoost', 'Neural Network'];
   public loading = false;
+  public horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  public verticalPosition: MatSnackBarVerticalPosition = 'top';
+  public missingValues: Record<string, number> = {};
+  public categoricalCols: string[] = []; // Colonne categoriali
+  public numericalCols: string[] = [];
+  previewData: any[] = [];
+  public selectedSplit: string = "holdout"; // Default: Holdout
+  public randomState: number = 42;
   private subscriptions: Array<Subscription> = [];
+  private _snackBar = inject(MatSnackBar);
 
   constructor(private sharedService: SharedService) {
+  }
+
+  public openSnackBar(message: string): void {
+    this._snackBar.open(message, 'x',{
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+    })
   }
 
   public onFileSelected(event: any) {
@@ -76,10 +98,15 @@ export class HomeComponent {
       tap(res => {
         console.log(res);
         this.loading = false;
+        this.missingValues = res?.missing_values;
+        this.categoricalCols = res?.categorical_cols;
+        this.numericalCols = res?.numerical_cols;
+        this.previewData = res?.preview_data;
       }),
       catchError(err => {
         console.log(err);
         this.loading = false;
+        this.openSnackBar('Si è verificato un errore durante la comunicazione con il server. Riprova più tardi.')
         return throwError(err);
       })
     ).subscribe()
@@ -97,4 +124,6 @@ export class HomeComponent {
       return (size / (1024 * 1024 * 1024)).toFixed(2) + ' GB'; // Gigabyte
     }
   }
+
+  protected readonly Object = Object;
 }
